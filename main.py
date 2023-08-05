44a5fcdfdf2e11140bdc30f9cb8eddadc5b9ab50
FII14 <121723975+FII14@users.noreply.github.com>
2023-08-05 23:36:24 +0700

    Update main.py

--- main.py
+++ main.py
@@ -24,21 +24,22 @@ if not os.path.exists(file_wordlist):
     sys.exit(1)
 
 try:
-    with open(file_wordlist, 'r', encoding="latin-1", errors="ignore") as daftar_kata_sandi:
+    with open(file_wordlist, "r", encoding="latin-1", errors="ignore") as daftar_kata_sandi:
         for kata_sandi in daftar_kata_sandi:
             kata_sandi = kata_sandi.strip()
-            perintah = ['steghide', 'extract', '-sf', file_steganografi, '-p', kata_sandi, '-f']
+            perintah = ["steghide", "extract", "-sf", file_steganografi, "-p", kata_sandi, "-f"]
             hasil = subprocess.run(perintah, capture_output=True, text=True)
 
             if hasil.returncode == 0:
                 file_terpecahkan = f"{file_steganografi}.out"
-                perintah_extract = ['steghide', 'extract', '-sf', file_steganografi, '-p', kata_sandi, '-xf', file_terpecahkan]
+                perintah_extract = ["steghide", "extract", "-sf", file_steganografi, "-p", kata_sandi, "-xf", file_terpecahkan]
                 subprocess.run(perintah_extract, capture_output=True, text=True)
-                print(f"\n[*] File steganografi: {file_steganografi}\n[*] Kata sandi: {kata_sandi}\n[*] Status: Benar\n")
+                print(f"\n[*] File Steganografi: {file_steganografi}\n[*] Kata Sandi: {kata_sandi}\n[*] Status: Benar\n")
                 break
             else:
-                print(f"\n[*] File steganografi: {file_steganografi}\n[*] Kata sandi: {kata_sandi}\n[*] Status: Salah")
+                print(f"\n[*] File Steganografi: {file_steganografi}\n[*] Kata Sandi: {kata_sandi}\n[*] Status: Salah")
+            time.sleep(1)
         else:
-            print(f"Kata sandi tidak ditemukan dalam daftar kata sandi '{file_wordlist}'.")
+            print(f"Kata sandi tidak ditemukan dalam file wordlist '{file_wordlist}'.")
 except Exception as e:
     print(f"Terjadi kesalahan: {e}")
