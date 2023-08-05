000caec408ba39aff31a8cf92393b8833464dfae
FII14 <121723975+FII14@users.noreply.github.com>
2023-08-05 23:29:40 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,32 +1,41 @@
-import os
-import time
 import subprocess
 
-file_stegano = input("Masukkan nama file steganografi: ")
+print("""
+@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+@ Program : Pemecah Kata Sandi File Steganografi    @
+@ Pembuat : Rofi [FII14]                            @
+@ GitHub  : https://github.com/FII14/PKSFS          @
+@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+""")
+
+file_steganografi = input("Masukkan nama file steganografi: ")
+
+if not os.path.exists(file_steganografi):
+    print(f"Kesalahan: File steganografi '{file_steganografi}' tidak ditemukan.")
+    sys.exit(1)
 
 file_wordlist = input("Masukkan nama file wordlist: ")
 
+if not os.path.exists(file_wordlist):
+    print(f"Kesalahan: File wordlist '{file_wordlist}' tidak ditemukan.")
+    sys.exit(1)
 
 try:
-    with open(file_wordlist, 'r') as w:
-        for baris in w:
-            kata_sandi = baris.strip()
-            command = ['steghide', 'extract', '-sf', file_stegano, '-p', kata_sandi, '-f']
-            result = subprocess.run(command, capture_output=True, text=True)
+    with open(file_wordlist, 'r') as daftar_kata_sandi:
+        for kata_sandi in daftar_kata_sandi:
+            kata_sandi = kata_sandi.strip()
+            perintah = ['steghide', 'extract', '-sf', file_steganografi, '-p', kata_sandi, '-f']
+            hasil = subprocess.run(perintah, capture_output=True, text=True)
 
-            if result.returncode == 0:
-                cracked_file = f"{file}.out"
-                command_s = ['steghide', 'extract', '-sf', file, '-p', password, '-xf', cracked_file]
-                subprocess.run(command_s, capture_output=True, text=True)
-                print(f"\n[*] File zip: {file_zip}\n[*] Kata sandi: {password}\n[*] Status: Benar\n")
+            if hasil.returncode == 0:
+                file_terpecahkan = f"{file_steganografi}.out"
+                perintah_extract = ['steghide', 'extract', '-sf', file_steganografi, '-p', kata_sandi, '-xf', file_terpecahkan]
+                subprocess.run(perintah_extract, capture_output=True, text=True)
+                print(f"\n[*] File steganografi: {file_steganografi}\n[*] Kata sandi: {kata_sandi}\n[*] Status: Benar\n")
                 break
-                sys.exit(0)
             else:
-                print(f"\n[*] File zip: {file_zip}\n[*] Kata sandi: {password}\n[*] Status: Salah")
+                print(f"\n[*] File steganografi: {file_steganografi}\n[*] Kata sandi: {kata_sandi}\n[*] Status: Salah")
         else:
-            print(f"Kata sandi tidak ditemukan dalam file wordlist '{file_wordlist}'.")
-            sys.exit(1)
-            
+            print(f"Kata sandi tidak ditemukan dalam daftar kata sandi '{file_wordlist}'.")
 except Exception as e:
     print(f"Terjadi kesalahan: {e}")
-    sys.exit(1)
