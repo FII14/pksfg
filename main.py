25fcf80365ab8d0b58e4ed831822303ed39a9c3f
FII14 <121723975+FII14@users.noreply.github.com>
2023-08-05 22:51:51 +0700

    Update main.py

--- main.py
+++ main.py
@@ -4,10 +4,18 @@ import sys
 
 gpg = gnupg.GPG()
 
+print("""
+@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+@ Program : Pemecah Kata Sandi File GPG    @
+@ Pembuat : Rofi [FII14]                   @
+@ GitHub  : https://github.com/FII14/PKSFG @
+@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+""")
+
 file_gpg = input("Masukkan nama file GPG: ")
 
 if not os.path.exists(file_gpg):
-    print(f"Kesalahan: File gpg '{file_gpg}' tidak ditemukan.")
+    print(f"Kesalahan: File GPG '{file_gpg}' tidak ditemukan.")
     sys.exit(1)
 
 file_wordlist = input("Masukkan nama file wordlist: ")
@@ -16,17 +24,15 @@ if not os.path.exists(file_wordlist):
     print(f"Kesalahan: File wordlist '{file_wordlist}' tidak ditemukan.")
     sys.exit(1)
 
-with open(file_wordlist, "r", encoding="latin-1", error="ignore") as f:
+with open(file_wordlist, "r", encoding="latin-1", errors="ignore") as f:
     daftar_kata_sandi = f.read().splitlines()
 
 for kata_sandi in daftar_kata_sandi:
-    print(f"Mencoba kata sandi: {kata_sandi}")
-
-    with open(filename, 'rb') as f:
-        decrypted_data = gpg.decrypt_file(f, passphrase=passphrase)
+    print(f"\n[*] File GPG: {file_gpg}\n[*] Kata sandi: {kata_sandi}\n[*] Status: Salah")
+    with open(file_gpg, 'rb') as f:
+        decrypted_data = gpg.decrypt_file(f, passphrase=kata_sandi)
         if decrypted_data.ok:
-            print(f"\n[*] File gpg: {file_gpg}\n[*] Kata sandi: {kata_sandi}\n[*] Status: Benar")
-            break
+            print(f"\n[*] File GPG: {file_gpg}\n[*] Kata sandi: {kata_sandi}\n[*] Status: Benar\n")
             sys.exit(0)
-        else:
-            print(f"\n[*] File gpgp: {file_gpg}\n[*] Kata sandi: {kata_sandi}\n[*] Status: Salah")
+
+print(f"Kata sandi tidak ditemukan dalam file wordlist '{file_wordlist}'.")
